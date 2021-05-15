65561ed8ae6baed75f97d3bb7e835c129dc2cf47
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2021-05-15 19:33:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# triplecheck-example-lambda
+
+## TripleCheck broker running on AWS Lambda and DynamoDB
+
+This repo demonstrates a working, basic implementation of a [TripleCheck broker](https://github.com/mikaelvesavuori/triplecheck-broker) running on AWS Lambda with the database in DynamoDB (pay-as-you-go model).
+
+Refer to the documentation on the [broker](https://github.com/mikaelvesavuori/triplecheck-broker) for how to call the API.
+
+Technology choices are:
+
+- [Node.js](https://nodejs.org/en/)
+- [TypeScript](https://www.typescriptlang.org)
+- [Serverless Framework](https://www.serverless.com) for easing deployment and packaging
+- [Webpack](https://webpack.js.org) for bundling
+
+## Pre-requisites
+
+- An AWS account
+- AWS credentials in your environment so that Serverless Framework can pick them up
+- Sufficient credentials to deploy and remove Lambda, API Gateway, S3 and DynamoDB
+- Configure the `custom` block in `serverless.yml` so it uses your own values
+
+## Installation
+
+Run `npm install` or `yarn install`.
+
+## Local development
+
+Run `npm start` or `yarn start`.
+
+## Deploy
+
+Run `npm run deploy` or `yarn run deploy`.
+
+## Teardown (remove stack)
+
+Run `npm run remove` or `yarn run remove`.
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { TripleCheckBroker } from 'triplecheck-broker';
+import { DynamoRepository } from 'triplecheck-repository-dynamodb';
+
+/**
+ * @description The Lambda function handler that will run the broker.
+ */
+export async function handler(event: any) {
+  const [request, payload] = await getRequestData(event);
+
+  const repository = new DynamoRepository();
+  const { responseData, status, headers } = await TripleCheckBroker(request, payload, repository);
+
+  const response = {
+    statusCode: status,
+    body: JSON.stringify(responseData),
+    headers
+  };
+
+  return response;
+}
+
+/**
+ * @description Utility function to get the data we need to run the TripleCheck broker.
+ * Expects the full AWS Lambda event object.
+ */
+async function getRequestData(event: any): Promise<any> {
+  const { body, httpMethod, path, queryStringParameters } = event;
+
+  const payload = body && typeof body === 'string' ? JSON.parse(body) : body;
+
+  const search = (() => {
+    let _search = '';
+    if (queryStringParameters && JSON.stringify(queryStringParameters) !== '{}') {
+      _search += Object.keys(queryStringParameters)[0];
+      _search += Object.values(queryStringParameters)[0];
+    }
+    return _search;
+  })();
+
+  return [
+    {
+      method: httpMethod,
+      pathname: path || '/',
+      search
+    },
+    payload
+  ];
+}
